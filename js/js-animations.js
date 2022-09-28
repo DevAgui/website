dca8aca520394d2d141212f1f52f477a1920e47a
DevAgui <dagmartin98@gmail.com>
2022-09-28 18:28:24 +0200

    first commit of my webpage

---
+++ js/js-animations.js
@@ -0,0 +1,6 @@
+
+window.addEventListener('load', (moveElement) => {
+  
+  document.getElementsByClassName('placeholder')[0].style.width="75%";
+  document.getElementsByClassName('placeholder')[0].style.transition="width 2s";
+});
